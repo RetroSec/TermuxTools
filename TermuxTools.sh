ac0a57836b901ae113c7c3b3242ec1cb28a8e8dc
ERROR 404 <67592808+RetroSec@users.noreply.github.com>
2020-09-03 10:45:44 +0700

    Update TermuxTools.sh

--- TermuxTools.sh
+++ TermuxTools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pkg update && pkg upgrade && pkg install root-repo unstable-repo x11-repo && apt install -y zsh figlet toilet metasploit python curl perl hydra sqlmap crunch golang && pip install lolcat cowsay && wget -O install-nethunter-termux https://offs.ec/2MceZWr && chmod +x install-nethunter-termux && ./install-nethunter-termux && git clone https://github.com/adi1090x/termux-style tstyle && cd tstyle && chmod +x install && bash install
+pkg update && pkg upgrade && pkg install root-repo unstable-repo x11-repo && apt install -y zsh nmap dnsutils coreutils figlet toilet metasploit python curl perl hydra sqlmap crunch golang && pip install lolcat cowsay && wget -O install-nethunter-termux https://offs.ec/2MceZWr && chmod +x install-nethunter-termux && ./install-nethunter-termux && git clone https://github.com/adi1090x/termux-style tstyle && cd tstyle && chmod +x install && bash install
 echo -e "\e[1;92m:: \eTermux installed completed ::\n"
