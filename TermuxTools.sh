eca29bdaf0f035b3687cda9554e3e10b31141c81
ERROR 404 <67592808+RetroSec@users.noreply.github.com>
2020-09-03 10:44:57 +0700

    Update TermuxTools.sh

--- TermuxTools.sh
+++ TermuxTools.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-def="\e[1;"
-red="${def}91m"
-cyan="${def}96m"
-yellow="${def}93m"
-green="${def}92m"
-reset="${def}0m"
-
-author="RetroSec | ERROR 404"
-
-pkg install -y pip figlet && pip install lolcat
-
-clear && figlet -f small TermuxTools | lolcat
-echo -e $green"เขียนโดย: ${cyan}$author"
-
-pkg update && pkg upgrade && pkg install -y python perl curl dnsutils coreutils nmap hydra metasploit && wget -O install-nethunter-termux https://offs.ec/2MceZWr && chmod +x install-nethunter-termux && ./install-nethunter-termux && rm -r TermuxTools && git clone https://github.com/adi1090x/termux-style.git && cd termux-style && chmod +x install && ./install && mv .bashrc /$HOME/.bash
+pkg update && pkg upgrade && pkg install root-repo unstable-repo x11-repo && apt install -y zsh figlet toilet metasploit python curl perl hydra sqlmap crunch golang && pip install lolcat cowsay && wget -O install-nethunter-termux https://offs.ec/2MceZWr && chmod +x install-nethunter-termux && ./install-nethunter-termux && git clone https://github.com/adi1090x/termux-style tstyle && cd tstyle && chmod +x install && bash install
+echo -e "\e[1;92m:: \eTermux installed completed ::\n"
